ac1d3b5daa44f01de092223df0f96fe602f8cb8c
Luca Datti <datti.luca@gmail.com>
2023-09-19 15:53:11 +0200

    aggiunta funzionalità passaggio parametri

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+// questa libreria ci permette di passare
+// argomenti al programma
+
+
 fn main() {
-    println!("Hello, world!");
+    
+    let args: Vec<String> = env::args().collect();
+
+/*
+    args() restituisce un Iterator, un oggetto
+    che possiamo manipolare tramite un "ciclo"
+
+    collect trasforma l'iterator
+    in una struttura tipo vettore, contenente 
+    gli effettivi argomenti passati, in successione
+*/
+
+
+// salviamo gli argomenti passati
+    
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {} in file {}",query,file_path);
 }
